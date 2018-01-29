567adc0768952b058ce92615a255196b10398b30
Tatsiujin Chin <c910335@gmail.com>
2018-01-29 17:01:53 +0800

    simple spec

--- spec/crinder_spec.cr
+++ spec/crinder_spec.cr
@@ -1,9 +1,24 @@
 require "./spec_helper"
 
-describe Crinder do
-  # TODO: Write tests
+record Todo, name : String, priority : Int32, expires_at : Time?, created_at : Time?, updated_at : Time?
 
-  it "works" do
-    false.should eq(true)
+class TodoRenderer < Crinder::Base(Todo)
+  field name : String, as: title
+  field priority : Int, filter: ->{ object.priority * 10 }
+  field expires_at : String, as: deadline, unless: ->{ object.priority < 3 }
+  field created_at : String, if: ->{ object.priority > 5 }
+  field updated : Bool, filter: updated?
+
+  def self.updated?
+    !object.updated_at.nil?
+  end
+end
+
+describe Crinder::Base do
+  it "converts object to json" do
+    time = Time.new(2018, 1, 29, 15, 23, 15)
+    t = Todo.new("www", 8, time + 20.hours, time, nil)
+
+    TodoRenderer.render(t).should eq(%({"title":"www","priority":80,"deadline":"2018-01-30 11:23:15","created_at":"2018-01-29 15:23:15","updated":false}))
   end
 end
